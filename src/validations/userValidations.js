7d8008b3f538084a76b663307e6c11c11feab80c
EmaSaucedo <lopeposo748@gmail.com>
2026-08-23 19:45:13 -0300

    Se agregó validacion de email unico

--- src/validations/userValidations.js
+++ src/validations/userValidations.js
@@ -1,4 +1,5 @@
 import { body } from "express-validator";
+import { modelUser } from "../model/modelUser.js";
 
 export const userValidations = [
     body("name")
@@ -6,11 +7,21 @@ export const userValidations = [
         .withMessage("El nombre es obligatorio"),
 
     body("email")
-        .notEmpty()
-        .withMessage("El email es obligatorio")
-        .isEmail()
-        .withMessage("El email no es valido")
-        ,
+    .notEmpty()
+    .withMessage("El email es obligatorio")
+    .isEmail()
+    .withMessage("El email no es valido")
+    .custom(async (email) => {
+        const user = await modelUser.findOne({
+            where: { email: email }
+        });
+
+        if (user) {
+            throw new Error("El email ya esta registrado");
+        }
+
+        return true;
+    }),
     body("password")
     .notEmpty()
     .withMessage("La contraseña es obligatoria")
