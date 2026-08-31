e3131958301e5d00dfef355cc63c89f2a84728ad
SaucedoEmanuel <lopeposo748@gmail.com>
2026-08-31 10:18:55 -0300

    fix: Se soluciono un error de tipeo en las rutas de personal data

--- src/router/personalDataRouter.js
+++ src/router/personalDataRouter.js
@@ -1,16 +1,13 @@
-import { personalDataValidations, updatePersonalDataValidations } from "../validations/personalDataValidations.js";
-
+import { personalDataValidations, updatePersonalDataValidations} from "../validations/personalDataValidations.js";
 import { validate } from "../middlewares/validate.js";
-
 import express from "express";
 
-import { createPersonalData, deletePersonalData, getPersonalData, updatePersonalData, getPersonalDataById } from "../controller/personalDataController.js";
+import { createPersonalData, getPersonalData, getPersonalDataById, updatePersonalData, deletePersonalData } from "../controller/personalDataController.js";
 
 export const router = express.Router();
+
 router.post("/", personalDataValidations, validate, createPersonalData);
 router.get("/", getPersonalData);
-router.get("/:id", getPersonalDataById);
-router.put("/:id", updatePersongit alDataValidations, validate, updatePersonalData);
-router.delete("/:id", deletePersonalData);
-
-
+router.get("/:id", getPersonalDataById)
+router.put("/:id", updatePersonalDataValidations, validate, updatePersonalData )
+router.delete("/:id", deletePersonalData)
